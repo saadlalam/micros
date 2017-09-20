307a5e6965c61a5822b44c7502d3cc6cf023452a
SAAD LALAOUI LAMDEGHERI <saad.lalaoui.lamdegheri@gmail.com>
2017-09-20 18:35:40 +0100

    first

---
+++ curr_prices.py
@@ -0,0 +1,32 @@
+import json
+import requests
+import time
+from bs4 import BeautifulSoup as bs
+'''
+A small crawler to get currency live prices. (it works for other tickers,
+just needs a small tweak)
+Author :Saadlalam
+Contact : saad.lalaoui.lamdegheri@gmail.com
+--------
+This is for educational purpose only.
+Please note that it is illegal to spam requests if the web app does not allow so.
+'''
+URL = "https://finance.google.com/finance?q="
+
+def live_quotes(quote, base):
+    
+    result = dict()
+    page = requests.get(URL+str(base.upper())+str(quote.upper()))
+    data = page.content
+    s = bs(data, 'html.parser')
+    s = s.find('span', attrs={'class': 'bld'})
+    s = s.text.split(' ')
+
+    result = {
+        'base' : base,
+        'quote': quote,
+        'bid': s[0]
+    }
+    return result
+
+
